e85b6497c40de168a6a2bb437a06a9fbc14b1069
jluwn <1404553845@qq.com>
2020-04-25 15:23:31 +0800

    ning的社区实验项目2020/04/25

--- src/main/java/life/ning/community/controller/IndexController.java
+++ src/main/java/life/ning/community/controller/IndexController.java
@@ -1,6 +1,7 @@
 package life.ning.community.controller;
 
 
+import life.ning.community.dto.PaginationDTO;
 import life.ning.community.dto.QuestionDTO;
 import life.ning.community.mapper.QuestionMapper;
 import life.ning.community.mapper.UserMapper;
@@ -19,27 +20,15 @@ import java.util.List;
 
 @Controller
 public class IndexController {
-    @Autowired
-    private UserMapper userMapper;
+
     @Autowired
     private QuestionService questionService;
     @GetMapping("/")
-    public String index(HttpServletRequest request,
-                        Model model){
-        Cookie[] cookies = request.getCookies();
-        if(cookies!=null&&cookies.length!=0)
-        for(Cookie cookie : cookies){
-            if(cookie.getName().equals("token")){
-                String token = cookie.getValue();
-                User user = userMapper.findByToken(token);
-                if (user != null) {
-                    request.getSession().setAttribute("user",user);
-                }
-                break;
-            }
-        }
-        List<QuestionDTO> questionList= questionService.list();
-        model.addAttribute("questions",questionList);
+    public String index(Model model,
+                        @RequestParam(name = "page", defaultValue = "1") Integer page,
+                        @RequestParam(name = "size", defaultValue = "5") Integer size){
+        PaginationDTO pagination= questionService.list(page,size);
+        model.addAttribute("pagination",pagination);
         return "index";
 
     }
--- src/main/java/life/ning/community/service/QuestionService.java
+++ src/main/java/life/ning/community/service/QuestionService.java
@@ -1,5 +1,6 @@
 package life.ning.community.service;
 
+import life.ning.community.dto.PaginationDTO;
 import life.ning.community.dto.QuestionDTO;
 import life.ning.community.mapper.QuestionMapper;
 import life.ning.community.mapper.UserMapper;
@@ -19,9 +20,30 @@ public class QuestionService {
     private QuestionMapper questionMapper;
     @Autowired
     private UserMapper userMapper;
-    public List<QuestionDTO> list() {
-        List<Question> questions = questionMapper.list();
+    public PaginationDTO list(Integer page, Integer size) {
+        PaginationDTO paginationDTO=new PaginationDTO();
+
+        Integer totalPage;
+        Integer totalCount = questionMapper.count();
+        if (totalCount % size == 0) {
+            totalPage = totalCount / size;
+        } else {
+            totalPage = totalCount / size + 1;
+        }
+
+        if (page < 1) {
+            page = 1;
+        }
+        if (page > totalPage) {
+            page = totalPage;
+        }
+
+        paginationDTO.setPagination(totalPage,page);
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.list(offset,size);
         List<QuestionDTO> questionDTOList = new ArrayList<>();
+
         for (Question question : questions) {
             User user = userMapper.findById(question.getCreator());
             QuestionDTO questionDTO = new QuestionDTO();
@@ -29,6 +51,40 @@ public class QuestionService {
             questionDTO.setUser(user);
             questionDTOList.add(questionDTO);
         }
-        return questionDTOList;
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
+    }
+
+    public PaginationDTO list(Integer userId, Integer page, Integer size) {
+        PaginationDTO paginationDTO=new PaginationDTO();
+        Integer totalPage;
+        Integer totalCount = questionMapper.countByUserId(userId);
+        if (totalCount % size == 0){
+            totalPage = totalCount / size;
+        }else {
+            totalPage = totalCount / size + 1;
+        }
+        if(page<1){
+            page=1;
+        }
+        if(page > totalPage){
+            page = totalPage;
+        }
+
+        paginationDTO.setPagination(totalPage, page);
+
+        Integer offset = size * (page - 1);
+        List<Question> questions = questionMapper.listByUserId(userId,offset,size);
+        List<QuestionDTO> questionDTOList = new ArrayList<>();
+
+        for (Question question : questions) {
+            User user = userMapper.findById(question.getCreator());
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(question,questionDTO);
+            questionDTO.setUser(user);
+            questionDTOList.add(questionDTO);
+        }
+        paginationDTO.setQuestions(questionDTOList);
+        return paginationDTO;
     }
 }
