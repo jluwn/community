0f24cbd9375f6e0ab2599db87b6f4343e017f65e
jluwn <1404553845@qq.com>
2020-05-08 23:44:49 +0800

    ning的社区实验 2020/05/08 23:45

--- src/main/java/life/ning/community/mapper/QuestionExtMapper.java
+++ src/main/java/life/ning/community/mapper/QuestionExtMapper.java
@@ -9,5 +9,11 @@ import java.util.List;
 
 public interface QuestionExtMapper {
     int incView(Question record);
+
     int incCommentCount(Question record);
+
+    List<Question> selectRelated(Question question);
+    // Integer countBySearch(QuestionQueryDTO questionQueryDTO);
+
+    // List<Question> selectBySearch(QuestionQueryDTO questionQueryDTO);
 }
